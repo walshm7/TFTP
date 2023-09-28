d5e92b8bc3a0ce78864f12b83d39ac8134711b06
mwampner <71980700+mwampner@users.noreply.github.com>
2023-09-28 10:38:29 -0400

    functional? rrq and error functions

--- temp.c
+++ temp.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,93 +16,107 @@
 #define CONNECTION_TIMEOUT 10
 
 // Function to send DATA packet
-void send_data(int sockfd, struct sockaddr_in * client_address, int block_num, char * data, int data_len){
+void send_data(int sockfd, struct sockaddr_in * client_address, int block_num, char * data, int data_len, socklen_t client_len){
+    printf("DATA SEND\n");
     // Build packet
-    char *packet = calloc(4+data_len, sizeof(char));
-    *packet = htons(3);
+    char *packet = malloc((4+data_len)* sizeof(char));
+    char * pos = packet;
+    *(short int*) pos = htons(3);
+    pos += sizeof(short int);
+    *(short int*) pos = htons(block_num);
+    pos += sizeof(short int);
+    strcpy(pos, data);
+    /*packet = htons(3);
     packet +=2;
     *packet = htons(block_num);
-    strcat(packet, data);
+    strcat(packet, data);*/
 
     // Send packet
-    sendto(sockfd, packet, 4+data_len, 0, (struct sockaddr *) client_address, sizeof(client_address));
+    sendto(sockfd, packet, 4+data_len, 0, (struct sockaddr *) client_address, client_len);
 
     // Free packet
     free(packet);
 }
 
 // Function to send ERROR
-void send_error(int sockfd, struct sockaddr_in * client_address, int error_code){
+void send_error(int sockfd, struct sockaddr_in * client_address, int error_code, socklen_t client_len){
     char * packet; 
+    char * pos;
     char * msg;
     int packet_size = 5; // opcodes and end byte
     int msg_size;
     if(error_code == 0){ // Not defined (11), see error message
         packet_size += 11;
         msg_size = 11;
-        msg = calloc(msg_size, sizeof(char));
-        strcat(msg, "Not defined");
+        msg = malloc(msg_size*sizeof(char));
+        strcpy(msg, "Not defined\0");
     }
     if(error_code == 1){ // File not found (14)
         packet_size += 14;
         msg_size = 14;
-        msg = calloc(msg_size, sizeof(char));
-        strcat(msg, "File not found");
+        msg = malloc(msg_size* sizeof(char));
+        strcpy(msg, "File not found\0");
     }
     if(error_code == 2){ // Access violation (16)
         packet_size += 16;
         msg_size = 16;
         msg_size = 11;
-        msg = calloc(msg_size, sizeof(char));
-        strcat(msg, "Access violation");        
+        msg = malloc(msg_size*sizeof(char));
+        strcpy(msg, "Access violation\0");        
     }
     if(error_code == 3){ // Disk full or allocation exceeded (33)
         packet_size += 33;
         msg_size = 33;
         msg_size = 11;
         msg = calloc(msg_size, sizeof(char));
-        strcat(msg, "Disk full or allocation exceeded");        
+        strcpy(msg, "Disk full or allocation exceeded\0");        
     }
     if(error_code == 4){ // Illegal TFTP operation (22)
         packet_size += 22;
         msg_size = 22;
         msg_size = 11;
         msg = calloc(msg_size, sizeof(char));
-        strcat(msg, "Illegal TFTP operation");
+        strcpy(msg, "Illegal TFTP operation\0");
     }
     if(error_code == 5){ // Unknown transfer ID (19)
         packet_size += 19;
         msg_size = 19;
         msg_size = 11;
-        msg = calloc(msg_size, sizeof(char));
-        strcat(msg, "Unknown transfer ID");
+        msg = malloc(msg_size* sizeof(char));
+        strcpy(msg, "Unknown transfer ID\0");
     }
     if(error_code == 6){ // File already exists (19)
         packet_size += 19;
         msg_size = 19;
         msg_size = 11;
-        msg = calloc(msg_size, sizeof(char));
-        strcat(msg, "File already exists");
+        msg = malloc(msg_size*sizeof(char));
+        strcpy(msg, "File already exists\0");
     }
     if(error_code == 7){ // No such user (12)
         packet_size += 12;
         msg_size = 12;
         msg_size = 11;
-        msg = calloc(msg_size, sizeof(char));
-        strcat(msg, "No such user");
+        msg = malloc(msg_size*sizeof(char));
+        strcpy(msg, "No such user\0");
     }
     // Create packet
-    packet[0] = htons(5);
-    packet[2] = htons(error_code);
-    strcat(packet, msg);
-    packet[packet_size] = '\0';
+    packet = malloc(packet_size * sizeof(char));
+    pos = packet;
+    *(short int*)pos = htons(5);
+    pos += sizeof(short int);
+    *(short int*)pos = htons(error_code);
+    pos += sizeof(short int);
+    strcpy(pos, msg);
 
     // Send packet
-    sendto(sockfd, packet, packet_size, 0, (struct sockaddr *) client_address, sizeof(client_address));
-
+    int c = sendto(sockfd, packet, packet_size, 0, (struct sockaddr *) client_address, client_len);
+    if(c < 0){
+        printf("Failed to send\n");
+        printf("ERROR: %s\n", strerror(errno));
+    }
     // Free packet and msg
     free(packet);
-    free(msg);
+    //free(msg);
 
     // Terminate connection (should just exit child)
     exit(1);
@@ -128,39 +141,36 @@ void send_ack(int sockfd, struct sockaddr_in * client_address, int block_num){
 // Function to handle RRQ (Read Request) (will call send_data?)
 void handle_rrq(int sockfd, struct sockaddr_in client_address, char *filename, socklen_t client_len) {
     // Implement RRQ (Read Request) handling here
-    printf("WHATTTTTTT: %s\n", filename);
     // Open file
-    FILE * read_file = fopen(filename, "r");
+    FILE * fp;
+    fp = fopen(filename, "r");
 
     // Send error for failed open file
-    if(!(read_file)){
-        printf("BAD FILE!!!\n");
-        send_error(sockfd, &client_address, 1);
+    if(!fp){
+        send_error(sockfd, &client_address, 1, client_len);
         exit(1);
     }
-
-
-    printf("??????: %d\n", read_file);
 
     // Start read
     int block_num = 1;
     int file_size;
-    fseek(read_file, 0, SEEK_END);
-    file_size = ftell(read_file);
-    fseek(read_file, 0, SEEK_SET);
+    fseek(fp, 0, SEEK_END);
+    file_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
     
     int fd = open(filename, 0);
     char * data;
-    printf("%d\n", file_size);
-    if(file_size <= 0){
-        send_error(sockfd, &client_address, 1);
+    printf("Read file: %d\n", file_size);
+    /*if(file_size <= 0){
+        send_error(sockfd, &client_address, 1, client_len);
         exit(1);
     }
-    else if(file_size < 511){
-        data = calloc(file_size + 1, sizeof(char));
-        fread(data, sizeof(char), file_size, read_file);
+    else*/ if(file_size < 511){
+        printf("Small file\n");
+        data = malloc((file_size + 1) * sizeof(char));
+        fread(data, sizeof(char), file_size, fp);
         data[file_size] = '\0';
-        send_data(sockfd, &client_address, block_num, data, file_size+1);
+        send_data(sockfd, &client_address, block_num, data, file_size+1, client_len);
 
         // Wait for ACK
         while(1){
@@ -187,18 +197,20 @@ void handle_rrq(int sockfd, struct sockaddr_in client_address, char *filename, s
             if (resend_result < 0 || select_result < 0) {
                 perror("Error in select");
                 close(fd);
-                fclose(read_file);
+                fclose(fp);
                 exit(1);
             } 
             else if (select_result == 0) {
                 // Connection timeout
                 printf("Connection timeout, aborting\n");
                 close(fd);
-                fclose(read_file);
+                fclose(fp);
                 exit(1);
-            }else if (resend_result == 0) {
+            }
+            if (resend_result == 0) {
                 // resend data
-                send_data(sockfd, &client_address, block_num, data, file_size+1);
+                printf("Resending data packet\n");
+                send_data(sockfd, &client_address, block_num, data, file_size+1, client_len);
                 continue;
             }
             
@@ -212,6 +224,29 @@ void handle_rrq(int sockfd, struct sockaddr_in client_address, char *filename, s
                 if (bytes_received < 0) {
                     perror("Error receiving request");
                 }
+                unsigned short opcode;
+                memcpy(&opcode, buffer, sizeof(unsigned short));
+                opcode = ntohs(opcode);
+                if(opcode == 4){
+                    printf("ACK received: block %d successfully transmitted\n", block_num);
+                }
+                else if(opcode == 5){
+                    printf("ERROR received\n");
+
+                }
+                else if(opcode < 4){
+                    // Unsupported opcode
+                    printf("Invalid code during transmission: %d\n", opcode);
+                    // Send ERROR packet
+                    send_error(sockfd, &client_address, 0, client_len);
+                }
+                else{
+                    // Unsupported opcode
+                    printf("Unsupported opcode: %d\n", opcode);
+                    // Send ERROR packet
+                    send_error(sockfd, &client_address, 4, client_len);
+                }
+                break;
             }
         }
 
@@ -219,13 +254,13 @@ void handle_rrq(int sockfd, struct sockaddr_in client_address, char *filename, s
         free(data);
     }
     else{ //require multiple reads
-        fclose(read_file);
+        fclose(fp);
         int bytes_remaining = file_size;
         while(bytes_remaining > 512){
-            data = calloc(512, sizeof(char));
+            data = malloc(512* sizeof(char));
             read(fd, data, 511);
             data[512] = '\0';
-            send_data(sockfd, &client_address, block_num, data, 512);
+            send_data(sockfd, &client_address, block_num, data, 512, client_len);
             // Set up timer for data timeout
             struct timeval data_timeout;
             data_timeout.tv_sec = DATA_TIMEOUT;
@@ -242,9 +277,9 @@ void handle_rrq(int sockfd, struct sockaddr_in client_address, char *filename, s
             FD_SET(sockfd, &read_fds);
 
             // 10 second timer
-            int select_result = select(sockfd + 1, &read_fds, NULL, NULL, &connection_timeout);
+            int select_result = select(sockfd+1, &read_fds, NULL, NULL, &connection_timeout);
 
-            int resend_result = select(sockfd + 1, &read_fds, NULL, NULL, &data_timeout);
+            int resend_result = select(sockfd+1, &read_fds, NULL, NULL, &data_timeout);
 
             if (resend_result < 0 || select_result < 0) {
                 perror("Error in select");
@@ -254,9 +289,11 @@ void handle_rrq(int sockfd, struct sockaddr_in client_address, char *filename, s
                 // Connection timeout
                 printf("Connection timeout, aborting\n");
                 exit(1);
-            }else if (resend_result == 0) {
+            }
+            if (resend_result == 0) {
                 // resend data
-                send_data(sockfd, &client_address, block_num, data, file_size+1);
+                printf("Resending data packet\n");
+                send_data(sockfd, &client_address, block_num, data, file_size+1, client_len);
                 continue;
             }
             //receive ack
@@ -269,19 +306,37 @@ void handle_rrq(int sockfd, struct sockaddr_in client_address, char *filename, s
                 if (bytes_received < 0) {
                     perror("Error receiving request");
                 }
-                printf("ACK received\n");
+
+                unsigned short opcode;
+                memcpy(&opcode, buffer, sizeof(unsigned short));
+                opcode = ntohs(opcode);
+                if(opcode == 4){
+                    printf("ACK received: block %d successfully transmitted\n", block_num);
+                }
+                else if(opcode == 5){
+                    printf("ERROR received\n");
+
+                }
+                else{
+                    // Unsupported opcode
+                    printf("Unsupported opcode: %d\n", opcode);
+                    // Send ERROR packet
+                    send_error(sockfd, &client_address, 4, client_len);
+                }
+                
             }
             free(data);
 
             block_num++;
             bytes_remaining -= 511;
+            
         }
 
         if(bytes_remaining != 0){
             data = calloc(bytes_remaining+1, sizeof(char));
             read(fd, data, bytes_remaining);
             data[bytes_remaining] = '\0';
-            send_data(sockfd, &client_address, block_num, data, bytes_remaining);
+            send_data(sockfd, &client_address, block_num, data, bytes_remaining, client_len);
             // Set up timer for data timeout
             struct timeval data_timeout;
             data_timeout.tv_sec = DATA_TIMEOUT;
@@ -307,15 +362,45 @@ void handle_rrq(int sockfd, struct sockaddr_in client_address, char *filename, s
                 exit(1);
             } else if (resend_result == 0) {
                 // resend data
-                send_data(sockfd, &client_address, block_num, data, file_size+1);
-            } else if (select_result == 0) {
+                printf("ACK not received resending block %d\n", block_num);
+                send_data(sockfd, &client_address, block_num, data, file_size+1, client_len);
+            }
+            if (select_result == 0) {
                 // Connection timeout
                 printf("Connection timeout, aborting\n");
                 exit(1);
             }
 
             // receive ACK
+            //receive ack
+            char buffer[MAX_BUFFER_SIZE];
+            if (FD_ISSET(sockfd, &read_fds)) {
+                // Receive request
+                memset(buffer, 0, sizeof(buffer));
+                ssize_t bytes_received = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&client_address, &client_len);
 
+                if (bytes_received < 0) {
+                    perror("Error receiving request");
+                }
+
+                unsigned short opcode;
+                memcpy(&opcode, buffer, sizeof(unsigned short));
+                opcode = ntohs(opcode);
+                if(opcode == 4){
+                    printf("ACK received: block %d successfully transmitted\n", block_num);
+                }
+                else if(opcode == 5){
+                    printf("ERROR received\n");
+
+                }
+                else{
+                    // Unsupported opcode
+                    printf("Unsupported opcode: %d\n", opcode);
+                    // Send ERROR packet
+                    send_error(sockfd, &client_address, 4, client_len);
+                }
+                
+            }
             free(data);
         }
 
@@ -395,12 +480,12 @@ int main(int argc, char *argv[]) {
     while (1) {
 
         // Set up timer for data timeout
-       /*struct timeval data_timeout;
+        struct timeval data_timeout;
         data_timeout.tv_sec = DATA_TIMEOUT;
-        data_timeout.tv_usec = 0;*/
+        data_timeout.tv_usec = 0;
 
         // Set up timer for connection timeout
-       /* struct timeval connection_timeout;
+        struct timeval connection_timeout;
         connection_timeout.tv_sec = CONNECTION_TIMEOUT;
         connection_timeout.tv_usec = 0;
 
@@ -420,7 +505,7 @@ int main(int argc, char *argv[]) {
             break;
         }
 
-        if (FD_ISSET(sockfd, &read_fds)) {*/
+        if (FD_ISSET(sockfd, &read_fds)) {
             // Receive request
             memset(buffer, 0, sizeof(buffer));
             ssize_t bytes_received = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&client_address, &client_len);
@@ -445,11 +530,18 @@ int main(int argc, char *argv[]) {
             if(rc == 0){
                 // Open new socket for connection
                 // Set the port for the server_address
+                int child_sock;
+                child_sock = socket(AF_INET, SOCK_DGRAM, 0);
+                if (child_sock < 0) {
+                    perror("Error opening socket");
+                    exit(1);
+                }
+
 
                 server_address.sin_port = htons(tid);
 
                 // Bind socket
-                /*if (bind(sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+                if (bind(child_sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
                     perror("Error binding");
                     tid++;
 
@@ -459,18 +551,16 @@ int main(int argc, char *argv[]) {
                     }
 
                     continue;
-                }*/
+                }
 
                 // Extract opcode
                 unsigned short opcode;
                 memcpy(&opcode, buffer, sizeof(unsigned short));
                 opcode = ntohs(opcode);
-                printf("%d\n", opcode);
                 if (opcode == 1) {
                     // RRQ - Read Request
-                    printf("%d: HELLO!!!!!!!!!!!\n", getpid());
                     char *filename = buffer + 2;
-                    handle_rrq(sockfd, client_address, filename, client_len);
+                    handle_rrq(child_sock, client_address, filename, client_len);
                 } else if (opcode == 2) {
                     // WRQ - Write Request
                     char *filename = buffer + 2;
@@ -479,10 +569,10 @@ int main(int argc, char *argv[]) {
                     // Unsupported opcode
                     printf("Unsupported opcode: %d\n", opcode);
                     // Send ERROR packet
-                    send_error(sockfd, &client_address, 4);
+                    send_error(sockfd, &client_address, 4, client_len);
                 }
             }
-        //}
+        }
     }
 
     close(sockfd);
